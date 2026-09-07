96b7119e78143456b6a95b6a442b7d9c39c68585
tranthanhthan1412 <tranthanhthang333@gmail.com>
2026-09-07 22:27:58 +0700

    feat(backend): integrate Swagger UI and add API documentation

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,8 @@ import messageRoute from './routes/messageRoute.js';
 import conversationRoute from './routes/conversationRoute.js';
 import { protectedRoute } from './middlewares/authMiddleware.js';
 import cors from "cors";
+import swaggerUi from 'swagger-ui-express';
+import fs from "fs";
 
 dotenv.config()
 
@@ -19,6 +21,12 @@ const PORT = process.env.PORT || 5001
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+//swagger 
+const swaggerDocument = JSON.parse(fs.readFileSync('./src/swagger.json', 'utf8'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
 // public routes
 app.use('/api/auth', authRoute)
 
